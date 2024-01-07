c3bd88e49391f51e4fd4822c4b5c71b7fb34d427
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2024-01-07 20:23:05 +0800

    add json add jngen for header

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "../include/jngen.h"
+#include "../include/json/json.h"
+using namespace std;
+
+struct object{
+	string _id, _class;
+	map<string, vector<string>> attr;
+};
+
+signed main(){
+	vector<object> obj;
+}
