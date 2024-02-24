8d63b4980e0c2f386eb14f61558afc56e9f06a40
cj.tsai <jimtsai9596@gmail.com>
2024-02-24 15:02:57 +0800

    generate array

--- src/array/array.cpp
+++ src/array/array.cpp
@@ -1,18 +1,34 @@
 #include<iostream>
 #include<string>
+#include<vector>
 #include"jngen.h"
 #include"array.hpp"
+#include"../number/number.hpp"
+#include"../graph/graph.hpp"
+#include"../string/string.hpp"
+#include"../tree/tree.hpp"
+#include"../geometry/geometry.hpp"
 using std::ostream, std::endl, std::cout;
 namespace MochaGen{
+    /*
     template<typename T>
-    struct gen_array{
-        gen_array(int _length): length(_length){}
-    };
+    gen_array<T>::gen_array(int _length, T _content): length(_length), content(_content), content_value(_length){}
+    template<typename T>
+    gen_array<T>& gen_array<T>::prepare(){
+        for(int i=0; i<length; i++){
+            content_value[i]=content;
+            content_value[i].prepare();
+        }
+    }*/
 
 }
-
+/*
 template<typename T>
 ostream& operator << (ostream& o, MochaGen::gen_array<T>& a){
-    o << a << std::endl;
+    a.prepare();
+    for(int i=0; i<a.length; i++){
+        //a.content.prepare();
+        o << a.content_value[i].len << std::endl;
+    }
 	return o;
-}
+}*/
--- src/array/array.hpp
+++ src/array/array.hpp
@@ -4,22 +4,52 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include"jngen.h"
+#include"array.hpp"
+#include"../number/number.hpp"
+#include"../graph/graph.hpp"
+#include"../string/string.hpp"
+#include"../tree/tree.hpp"
+#include"../geometry/geometry.hpp"
+using std::ostream, std::endl, std::cout;
 
 namespace MochaGen{
     template<typename T>
 	struct gen_array{
 		int length=0;
-        T element;
+        bool prepared=0;
+        T content;
+        std::vector<T> content_value;
         bool is_unique=0;
         bool is_sorted=0;
-        gen_array(int _length);
-		gen_array& intrange(int l, int r);
-        gen_array& floatrange(float l, float r);
+        /*
+        gen_array(int _length, T _content);
 		gen_array& prepare();
+        */
+        gen_array(int _length, T _content): length(_length), content(_content), content_value(_length, _content){
+            //content_value.reserve(_length);
+            //for(int i=0; i<length; i++) content_value.push_back(content);
+        }
+        gen_array<T>& prepare(){
+            prepared=1;
+            for(int i=0; i<length; i++){
+                content_value[i].prepare();
+            }
+            return *this;
+        }
 	};
 };
-
+/*
 template<typename T>
 std::ostream& operator << (std::ostream& o, MochaGen::gen_array<T>& a);
-
+*/
+template<typename T>
+ostream& operator << (ostream& o, MochaGen::gen_array<T>& a){
+    if(!a.prepared)a.prepare();
+    for(int i=0; i<a.length; i++){
+        //a.content.prepare();
+        o << a.content_value[i] << " ";
+    }
+	return o;
+}
 #endif
