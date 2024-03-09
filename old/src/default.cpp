3091a88ebd839759e65326e51fb1055dd0fbfad9
AaronWu-train <aaronwu0218@gmail.com>
2024-03-09 14:32:02 +0800

    remove old

--- old/src/default.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-#include"../include/jngen.h"
-using namespace std;
-
-signed main(){
-
--- old/test/number.sh
+++
@@ -1,5 +0,0 @@
-g++ -o num.o ../src/main.cpp
-touch ../output/number.cpp
-cat ../src/default.cpp > ../output/number.cpp
-./num.o >> ../output/number.cpp
-rm num.o
