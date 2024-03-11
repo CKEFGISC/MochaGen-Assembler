9655b98b051c865ed834c9963fa7210d00eb83b4
AaronWu-train <aaronwu0218@gmail.com>
2024-03-11 15:31:18 +0800

    Remove implement in header

--- src/array/array.cpp
+++ src/array/array.cpp
@@ -12,6 +12,32 @@
 #include "../tree/tree.hpp"
 using std::ostream, std::endl, std::cout;
 namespace MochaGen {
+
+template <typename T>
+gen_array<T>& gen_array<T>::end(std::string _endswith) {
+    endswith = _endswith;
+    return *this;
+}
+
+template <typename T>
+gen_array<T>& gen_array<T>::prepare() {
+    prepared = 1;
+    for (int i = 0; i < length; i++) {
+        content_value[i].prepare();
+    }
+    return *this;
+}
+
+template <typename T>
+ostream& operator<<(ostream& o, MochaGen::gen_array<T>& a) {
+    if (!a.prepared) a.prepare();
+    for (int i = 0; i < a.length; i++) {
+        // a.content.prepare();
+        o << a.content_value[i];
+    }
+    cout << a.endswith;
+    return o;
+}
 /*
 template<typename T>
 gen_array<T>::gen_array(int _length, T _content): length(_length), content(_content), content_value(_length){}
@@ -23,7 +49,7 @@ gen_array<T>& gen_array<T>::prepare(){
     }
 }*/
 
-}
+}  // namespace MochaGen
 /*
 template<typename T>
 ostream& operator << (ostream& o, MochaGen::gen_array<T>& a){
--- src/array/array.hpp
+++ src/array/array.hpp
@@ -8,6 +8,7 @@
 using std::ostream, std::endl, std::cout;
 
 namespace MochaGen {
+
 template <typename T>
 struct gen_array {
     int length = 0;
@@ -18,40 +19,14 @@ struct gen_array {
     bool is_sorted = 0;
     std::string endswith = "\n";
 
-    /*
-    gen_array(int _length, T _content);
-            gen_array& prepare();
-    */
-    gen_array(int _length, T _content) : length(_length), content(_content), content_value(_length, _content) {
-        // content_value.reserve(_length);
-        // for(int i=0; i<length; i++) content_value.push_back(content);
-    }
+    gen_array(int _length, T _content) : length(_length), content(_content), content_value(_length, _content) {}
 
-    gen_array<T>& end(std::string _endswith) {
-        endswith = _endswith;
-        return *this;
-    }
-    gen_array<T>& prepare() {
-        prepared = 1;
-        for (int i = 0; i < length; i++) {
-            content_value[i].prepare();
-        }
-        return *this;
-    }
-};
+    gen_array<T>& end(std::string _endswith);
+    gen_array<T>& prepare();
+};  // struct gen_array
+
 };  // namespace MochaGen
-/*
-template<typename T>
-std::ostream& operator << (std::ostream& o, MochaGen::gen_array<T>& a);
-*/
+
 template <typename T>
-ostream& operator<<(ostream& o, MochaGen::gen_array<T>& a) {
-    if (!a.prepared) a.prepare();
-    for (int i = 0; i < a.length; i++) {
-        // a.content.prepare();
-        o << a.content_value[i];
-    }
-    cout << a.endswith;
-    return o;
-}
+ostream& operator<<(ostream& o, MochaGen::gen_array<T>& a);
 #endif
